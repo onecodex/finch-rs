b369b0820757aed9a30bf2c155ebf3cf34c03f57
Vincent Prouillet <github@vincentprouillet.com>
2023-01-13 12:03:36 +0100

    Avoid creating an array for histogram
    Closes #63

--- lib/src/statistics.rs
+++ lib/src/statistics.rs
@@ -1,4 +1,5 @@
 use std::cmp;
+use std::collections::HashMap;
 
 use crate::sketch_schemes::KmerCount;
 
@@ -20,84 +21,105 @@ pub fn cardinality(sketch: &[KmerCount]) -> Result<u64, &'static str> {
 /// Generates a Vec of numbers of kmers for each coverage level
 ///
 /// For example, a size 1000 sketch of the same genome repeated 5 times (e.g. 5x coverage) should
-/// produce a "histogram" like [0, 0, 0, 0, 1000] (assuming no repetative kmers in the genome)
+/// produce a "histogram" like [0, 0, 0, 0, 1000] (assuming no repetitive kmers in the genome)
 ///
 pub fn hist(sketch: &[KmerCount]) -> Vec<u64> {
-    let mut counts = vec![0u64; 65536];
-    let mut max_count: u64 = 0;
+    let mut max_count = 0;
+    let mut counts: HashMap<usize, u64> = HashMap::with_capacity(150_000);
     for kmer in sketch {
         max_count = cmp::max(max_count, u64::from(kmer.count));
-        counts[kmer.count as usize - 1] += 1;
+        counts
+            .entry(kmer.count as usize - 1)
+            .and_modify(|c| *c += 1)
+            .or_insert(1);
     }
-    counts.truncate(max_count as usize);
-    counts
+
+    let mut counts_vec: Vec<u64> = Vec::with_capacity(max_count as usize);
+    for i in 0..max_count {
+        counts_vec.push(*counts.get(&(i as usize)).unwrap_or(&0))
+    }
+    counts_vec
 }
 
-#[test]
-fn test_hist() {
-    let sketch = vec![
-        KmerCount {
-            hash: 1,
-            kmer: vec![],
-            count: 1,
-            extra_count: 0,
-            label: None,
-        },
-        KmerCount {
-            hash: 2,
-            kmer: vec![],
-            count: 1,
-            extra_count: 0,
-            label: None,
-        },
-        KmerCount {
-            hash: 3,
-            kmer: vec![],
-            count: 1,
-            extra_count: 0,
-            label: None,
-        },
-    ];
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let hist_data = hist(&sketch);
-    assert_eq!(hist_data.len(), 1);
-    assert_eq!(hist_data[0], 3);
+    #[test]
+    fn test_hist() {
+        let sketch = vec![
+            KmerCount {
+                hash: 1,
+                kmer: vec![],
+                count: 1,
+                extra_count: 0,
+                label: None,
+            },
+            KmerCount {
+                hash: 2,
+                kmer: vec![],
+                count: 1,
+                extra_count: 0,
+                label: None,
+            },
+            KmerCount {
+                hash: 3,
+                kmer: vec![],
+                count: 1,
+                extra_count: 0,
+                label: None,
+            },
+        ];
 
-    let sketch = vec![
-        KmerCount {
-            hash: 1,
-            kmer: vec![],
-            count: 4,
-            extra_count: 0,
-            label: None,
-        },
-        KmerCount {
-            hash: 2,
-            kmer: vec![],
-            count: 2,
-            extra_count: 0,
-            label: None,
-        },
-        KmerCount {
-            hash: 3,
-            kmer: vec![],
-            count: 4,
-            extra_count: 0,
-            label: None,
-        },
-        KmerCount {
-            hash: 4,
-            kmer: vec![],
-            count: 3,
-            extra_count: 0,
-            label: None,
-        },
-    ];
+        let hist_data = hist(&sketch);
+        assert_eq!(hist_data.len(), 1);
+        assert_eq!(hist_data[0], 3);
 
-    let hist_data = hist(&sketch);
-    assert_eq!(hist_data.len(), 4);
-    assert_eq!(hist_data[0], 0);
-    assert_eq!(hist_data[1], 1);
-    assert_eq!(hist_data[2], 1);
-    assert_eq!(hist_data[3], 2);
+        let sketch = vec![
+            KmerCount {
+                hash: 1,
+                kmer: vec![],
+                count: 4,
+                extra_count: 0,
+                label: None,
+            },
+            KmerCount {
+                hash: 2,
+                kmer: vec![],
+                count: 2,
+                extra_count: 0,
+                label: None,
+            },
+            KmerCount {
+                hash: 3,
+                kmer: vec![],
+                count: 4,
+                extra_count: 0,
+                label: None,
+            },
+            KmerCount {
+                hash: 4,
+                kmer: vec![],
+                count: 3,
+                extra_count: 0,
+                label: None,
+            },
+            // https://github.com/onecodex/finch-rs/issues/63
+            KmerCount {
+                hash: 3,
+                kmer: vec![],
+                count: 126497,
+                extra_count: 0,
+                label: None,
+            },
+        ];
+
+        let hist_data = hist(&sketch);
+        assert_eq!(hist_data.len(), 126497);
+        assert_eq!(hist_data[0], 0);
+        assert_eq!(hist_data[1], 1);
+        assert_eq!(hist_data[2], 1);
+        assert_eq!(hist_data[3], 2);
+        assert_eq!(hist_data[126497 - 1], 1);
+    }
 }
