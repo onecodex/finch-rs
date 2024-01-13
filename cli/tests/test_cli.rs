84dbc100e3696f5ab32f3a08229c2f3a6a906a58
Austin Richardson <accounts@agdr.org>
2024-01-12 18:37:04 -0800

    Fix newlines in kmers

--- cli/tests/test_cli.rs
+++ cli/tests/test_cli.rs
@@ -93,6 +93,20 @@ fn finch_sketch_scaled() -> Result<(), Box<dyn std::error::Error>> {
     assert_eq!(sketch["kmer"], 21);
     assert_eq!(sketch["alphabet"], "ACGT");
     assert_eq!(sketch["sketchSize"], 10);
+
+    let kmers = &sketch["sketches"][0]["kmers"];
+
+    assert_eq!(kmers[0], "ATGCTAGCTACGTAACGTCGC");
+    assert_eq!(kmers[1], "CAGTCGATCGATCGTAGCTGA");
+    assert_eq!(kmers[2], "CTCAGATGCTGAGCCGGTCTA");
+    assert_eq!(kmers[3], "GCTAGCTAGCATCGCTAGCTA");
+    assert_eq!(kmers[4], "GACTAGCTAGCTAGCTAGCGA");
+    assert_eq!(kmers[5], "CGCTAGCTACGATCGATCGAC");
+    assert_eq!(kmers[6], "TAATTTATACGGGCCTATTAA");
+    assert_eq!(kmers[7], "GCATCAGCTAGCATCGCTGTA");
+    assert_eq!(kmers[8], "AGCCGGTCTACTACTACACAT");
+    assert_eq!(kmers[9], "AAGGCCTAACTTAATAGGCCC");
+
     //assert_eq!(sketch["max_hash"], usize::max_value() / 1000);
     assert_eq!(sketch["hashSeed"], 0);
 
--- lib/src/sketch_schemes/scaled.rs
+++ lib/src/sketch_schemes/scaled.rs
@@ -69,7 +69,7 @@ impl SketchScheme for ScaledSketcher {
         'seq: 'inner,
     {
         self.total_bases += seq.sequence().len() as u64;
-        let rc = seq.reverse_complement();
+        let rc = seq.normalize(false).reverse_complement();
         for (_, kmer, is_rev_complement) in
             seq.normalize(false).canonical_kmers(self.kmer_length, &rc)
         {
